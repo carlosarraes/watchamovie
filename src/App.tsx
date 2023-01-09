84d9615360daf074254a3812910d4d000f4103c5
carraes <carraeshb@gmail.com>
2023-01-09 01:52:51 -0300

    feat: setup Alert, mock jwtToken

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import { Link, Outlet } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, Outlet, useNavigate } from 'react-router-dom'
+import Alert from './components/Alert'
 
 function App() {
+  const [jwtToken, setJwtToken] = useState('')
+  const [alertMsg, setAlertMsg] = useState('')
+  const [alertClass, setAlertClass] = useState('hidden')
+
+  const navigate = useNavigate()
+
+  const logout = () => {
+    setJwtToken('')
+    navigate('/login')
+  }
+
   return (
     <>
       <header className="mx-2">
         <section className="flex justify-between items-center">
           <h1>Go Watch a Movie!</h1>
-          <Link to="/login">Login</Link>
+          {!jwtToken ? (
+            <Link
+              to="/login"
+              className="border py-1 px-3 rounded-md shadow-md bg-green-200 hover:bg-green-400 duration-200"
+            >
+              Login
+            </Link>
+          ) : (
+            <button
+              onClick={logout}
+              className="border py-1 px-3 rounded-md shadow-md bg-red-200 hover:bg-red-400 duration-200"
+            >
+              Logout
+            </button>
+          )}
         </section>
       </header>
       <main className="flex mx-2">
-        <nav className="flex flex-col w-2/12">
+        <nav className="flex flex-col w-3/12">
           <Link to="/">Home</Link>
           <Link to="/movies">Movies</Link>
           <Link to="/genres">Genre</Link>
-          <Link to="/admin/movie/0">Add Movie</Link>
-          <Link to="/admin">Catalogue</Link>
-          <Link to="/graphql">GraphQl</Link>
+          {jwtToken && <Link to="/admin/movie/0">Add Movie</Link>}
+          {jwtToken && <Link to="/admin">Catalogue</Link>}
+          {jwtToken && <Link to="/graphql">GraphQl</Link>}
         </nav>
-        <Outlet />
+        <section className="flex flex-col w-full justify-center">
+          <Alert message={alertMsg} className={alertClass} />
+          <Outlet
+            context={{
+              jwtToken,
+              setJwtToken,
+              setAlertMsg,
+              setAlertClass,
+            }}
+          />
+        </section>
       </main>
     </>
   )

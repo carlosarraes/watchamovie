dafbbcdf274681d65b4bd95c7b229d53e639b2e1
carraes <carraeshb@gmail.com>
2023-01-09 16:25:14 -0300

    fix: remove export

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
-export interface Movie {
+interface Movie {
   id: number
   title: string
   releaseDate: string

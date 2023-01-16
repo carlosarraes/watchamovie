9a0c9d3b39f9ed134a6b2de8670926b1a009f016
carraes <carraeshb@gmail.com>
2023-01-16 10:40:48 -0300

    feat: User model

---
+++ server/internal/models/User.go
@@ -0,0 +1,25 @@
+package models
+
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
+
+func (u *User) PasswordMatches(paintText string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(paintText))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+
+	return true, nil
+}
